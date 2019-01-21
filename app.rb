6bd40b362ef76a1ab457ffee2dac4f835627cdf8
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-21 21:20:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,26 @@ module FormsLab
       ship2 = Ship.new 
       @ships = [ship1, ship2]
       
+      post '/pirates' do 
       
+      @pirate = Pirate.new 
+      ship1 = Ship.new 
+      ship2 = Ship.new 
+      @ships = [ship1, ship2]
+      
+      @pirate.name = params[:pirate].name 
+      @pirate.height = params[:pirate].height 
+      @pirate.weight = params[:pirate].weight 
+      @ships[0].name = params[:pirate][ships][0][name]
+      @ships[0].type = params[:pirate][ships][0][type]
+      @ships[0].booty = params[:pirate][ships][0][booty]
+      @ships[1].name = params[:pirate][ships][1][name]
+      @ships[1].type = params[:pirate][ships][1][type]
+      @ships[1].booty = params[:pirate][ships][1][booty]
+      
+      erb :show
+      
+    end 
       
       erb :show
       
