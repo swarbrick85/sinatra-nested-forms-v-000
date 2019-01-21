3ef841212168a37cb21bf5e31ad1c28f8acebb89
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-21 21:31:51 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,9 +3,9 @@ class Pirate
   attr_accessor :name, :weight, :height, :ships 
   @@all = []
   
-  def initialize(p) 
+  def initialize(params) 
     @@all << self
-    
+    @name = 
   end
   
   def self.all 
